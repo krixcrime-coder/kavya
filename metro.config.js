09d96545c3dede63f4a296b551493dad80b7f487
krixcrime-coder <sync-tool@local>
2026-08-08 07:07:42 +0530

    Empty repo via github_sync_tool

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativewind } = require("nativewind/metro");
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts.push("mjs");
-
-module.exports = withNativewind(config);
